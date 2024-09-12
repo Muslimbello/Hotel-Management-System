1d0d117becee1f0532fb888448b0091e252bca8f
Muslimbello <yuusufyusasif@gmail.com>
2024-09-12 09:47:12 +0100

    updated user info

--- hotel_booking/app_auth/models.py
+++ hotel_booking/app_auth/models.py
@@ -4,21 +4,21 @@ from django.utils.translation import gettext_lazy as _
 
 
 class BlangoUserManager(UserManager):
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, username, password, **extra_fields):
         if not email:
             raise ValueError("Email must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, username=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, username, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, username=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -27,21 +27,21 @@ class BlangoUserManager(UserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, username, password, **extra_fields)
 
 
 class User(AbstractUser):
-    username = None  # Remove the username field
     email = models.EmailField(
-        _("email address"),
-        unique=True,
-    )
+        _("email address"), unique=True
+    )  # Email as unique identifier
     password = models.CharField(max_length=100)  # Ensure password field is present
 
     objects = BlangoUserManager()
 
-    USERNAME_FIELD = "email"  # Use email as the unique identifier for login
-    REQUIRED_FIELDS = []  # No additional fields are required besides email and password
+    USERNAME_FIELD = "email"  # Email as unique identifier for login
+    REQUIRED_FIELDS = [
+        "username"
+    ]  # Username is required in addition to email and password
 
     def __str__(self):
         return self.email
