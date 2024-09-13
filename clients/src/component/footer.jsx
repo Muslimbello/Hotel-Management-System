37519567b2f367b8eb9bcaff75753bc86adad61b
marcelinus <marcelinusilonuba21@gmail.com>
2024-09-13 23:34:09 +0100

    add footer

--- clients/src/component/footer.jsx
+++ clients/src/component/footer.jsx
@@ -6,7 +6,10 @@ const Footer = () => {
       <div className="footer_box">
         <div className="footer_box_card1">
           <h2>BG&C HOTEL</h2>
-          <p>This project don for learning porpose </p>
+          <p>
+            At our hotel resort, relaxation isn’t just a promise—it’s a
+            lifestyle.
+          </p>
         </div>
         <div className="footer_box_card">
           <h4>Contact Godswill</h4>
@@ -14,9 +17,9 @@ const Footer = () => {
             {" "}
             <i class="bi bi-telephone-forward"></i>+2348167828238
           </p>
-          <p>
-            <i class="bi bi-envelope-fill"></i>godswillchinedu644@gmail.com
-          </p>
+          <a href="#">
+            <i class="bi bi-envelope-fill"></i>
+          </a>
           <a href="#">
             <i class="bi bi-twitter-x"></i>
           </a>
@@ -35,9 +38,9 @@ const Footer = () => {
           <p>
             <i class="bi bi-telephone-forward"></i>+2349031292535
           </p>
-          <p>
-            <i class="bi bi-envelope-fill"></i>yuusufyusasif@gmail.com
-          </p>
+          <a href="#">
+            <i class="bi bi-envelope-fill"></i>
+          </a>
           <a href="#">
             <i class="bi bi-twitter-x"></i>
           </a>
@@ -56,23 +59,24 @@ const Footer = () => {
           <p>
             <i class="bi bi-telephone-forward"></i>+2348168945213
           </p>
-          <p>
-            <i class="bi bi-envelope-fill"></i>marcelinusilonuba21@gmail.com
-          </p>
+          <a href="#">
+            <i class="bi bi-envelope-fill"></i>
+          </a>
           <a href="#">
             <i class="bi bi-twitter-x"></i>
           </a>
-          <a href="#">
+          <a href="https://facebook.com/chukwudi.ilonuba/">
             <i class="bi bi-facebook"></i>
           </a>
           <a href="#">
             <i class="bi bi-instagram"></i>
           </a>
-          <a href="#">
+          <a href="https://www.linkedin.com/in/ilonuba-chukwudi-a14868191/">
             <i class="bi bi-linkedin"></i>
           </a>
         </div>
       </div>
+      <div className="last"> Copyright &copy; 2024 BG&C All Right Reserved</div>
     </div>
   );
 };
